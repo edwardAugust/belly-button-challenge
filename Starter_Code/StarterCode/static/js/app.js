e4cbe8af5fd230e922c9cf0b1174fabdbaf22454
edwardAugust <edward.aug.watts@gmail.com>
2023-04-04 18:12:58 -0500

    06april2023

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,14 +1,63 @@
+// fuctions
+function myFunction(ls) {
+  for (const x of [1,2,3,4,5]) {
+    // code block to be executed
+  }
+  return ;
+}
+
+// set up a sample chart
+var data = [{
+  type: 'bar',
+  x : [194, 178, 162, 92, 84, 40, 37, 28, 27, 24],
+  y: [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977].toString(),
+  orientation: 'h'
+}];
+
+Plotly.newPlot('plot', data);
+// assign variable lists
+
+
+let samplesList = []
+let sample_values = [] // 
+let otu_ids = [] // convert to string each elements
+let otu_labels =[]
+
 // Create variables that call the main 
 // dictionary keys from samples.json
 
-let item
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+// const map1 = array1.map(x => "OTU " + x.toString());
+let samples
+
+// for loop
+    // map within
 d3.json(url).then(function(data) {
-    console.log("inside")
-    console.log(data);
-    item = data
-    console.log("inside")
+    console.log("inside");
+    samples = data.samples;
+    console.log(samples);
+    
+    //let sample_values = samples.sample_values
+    //let otu_ids = [] // convert to string each elements
+    //let otu_labels =[]
+
+
+
+
+    //console.log(samples.sample_values);
+
+
+
+
+
+    for (let i = 0; i < samples.length; i++) {
+      row = samples[i];
+      // console.log("howdy")
+      samplesList.push(row);
+      // console.log(data.samples);
+    }
+
   });
 
 // 'names' : [list of ID numbers as strings]
@@ -22,6 +71,3 @@ d3.json(url).then(function(data) {
 
 // 'samples' : [list of dictionaries,
 //            'id', 'otu_ids', 'otu_labels']
-console.log('outside');
-console.log(item);
-
